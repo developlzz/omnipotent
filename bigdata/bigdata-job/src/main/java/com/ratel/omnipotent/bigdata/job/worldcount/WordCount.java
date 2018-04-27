eca712ed859906dcf2ec02c4362ea7c2dc96310c
LiZZ <lzzaifx@126.com>
2018-04-27 16:10:31 +0800

    1111

--- bigdata/bigdata-job/src/main/java/com/ratel/omnipotent/bigdata/job/worldcount/WordCount.java
+++ bigdata/bigdata-job/src/main/java/com/ratel/omnipotent/bigdata/job/worldcount/WordCount.java
@@ -1,8 +1,19 @@
 package com.ratel.omnipotent.bigdata.job.worldcount;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -50,6 +61,76 @@ public class WordCount {
         }  
     }  
 	
+	public static void deleteOutDir(Configuration conf, String OUT_DIR)  
+            throws IOException, URISyntaxException {  
+        FileSystem fileSystem = FileSystem.get(new URI(OUT_DIR), conf);  
+        if(fileSystem.exists(new Path(OUT_DIR))){  
+            fileSystem.delete(new Path(OUT_DIR), true);  
+        }  
+    }  
+	
+	public static String printValue(String fullPath, Configuration conf, String outPath, String outFileName) throws IOException {
+		if (StringUtils.isBlank(fullPath)) {  
+            return null;  
+        }  
+        String split = "/";
+        int idx = fullPath.indexOf(split, 10);
+        String root = fullPath.substring(0, idx);
+        String path = fullPath.substring(idx);
+        FileSystem fs = FileSystem.get(URI.create(root), conf);  
+        // check if the file exists  
+        Path p = new Path(path);  
+        if (fs.exists(p)) {  
+            FSDataInputStream is = fs.open(p);  
+            // get the file info to create the buffer  
+            FileStatus stat = fs.getFileStatus(p);  
+            // create the buffer  
+            byte[] buffer = new byte[Integer.parseInt(String.valueOf(stat.getLen()))];  
+            is.readFully(0, buffer);  
+            is.close();  
+            fs.close();  
+//            byte2File(buffer, outPath, outFileName);
+           String str = new String(buffer);
+           return str;
+        } else {  
+            throw new IOException("the file is not found .");  
+        }  
+	}
+	
+	public static void byte2File(byte[] bfile,String filePath,String fileName){  
+        BufferedOutputStream bos=null;  
+        FileOutputStream fos=null;  
+        File file=null;  
+        try{  
+            File dir=new File(filePath);  
+            if(!dir.exists() && !dir.isDirectory()){//判断文件目录是否存在    
+                dir.mkdirs();    
+            }  
+            file=new File(filePath+fileName);  
+            fos=new FileOutputStream(file);  
+            bos=new BufferedOutputStream(fos);  
+            bos.write(bfile);  
+        }   
+        catch(Exception e){  
+            System.out.println(e.getMessage());  
+            e.printStackTrace();    
+        }  
+        finally{  
+            try{  
+                if(bos != null){  
+                    bos.close();   
+                }  
+                if(fos != null){  
+                    fos.close();  
+                }  
+            }  
+            catch(Exception e){  
+                System.out.println(e.getMessage());  
+                e.printStackTrace();    
+            }  
+        }  
+    }  
+	
 	 /** 
      * 上面我们把map，reduce都写完了，下面我们把它们合在一起，运转起来 
      */  
@@ -59,11 +140,13 @@ public class WordCount {
     	System.setProperty("hadoop.home.dir", "D:\\hadoop\\hadoop-3.0.2");
         //加载驱动  
         Configuration conf = new Configuration();  
+        conf.set("fs.hdfs.impl",org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());    
         //获取job,告诉他需要加载那个类  
         Job job = Job.getInstance(conf, WordCount.class.getSimpleName());  
 
         //获取文件数据  
-        FileInputFormat.setInputPaths(job, new Path("hdfs://10.10.21.71:9000/test/words.txt"));  
+        FileInputFormat.setInputPaths(job, new Path("hdfs://10.10.21.71:9000/test/new_words.txt"));  
+        
         //通过TextInputFormat把读到的数据处理成<k1,v1>形式  
         job.setInputFormatClass(TextInputFormat.class);  
         //job中加入Mapper，同时MyMapper类接受<k1,v1>作为参数传给类中map函数进行数据处理  
@@ -81,8 +164,21 @@ public class WordCount {
         FileOutputFormat.setOutputPath(job, new Path(OUT_DIR));  
         job.setOutputFormatClass(TextOutputFormat.class);  
         //如果这个文件存在则删除，如果文件存在不删除会报错。  
-//        deleteOutDir(conf, OUT_DIR);  
+        deleteOutDir(conf, OUT_DIR);  
         //把处理好的<k3,v3>的数据写入文件  
+        
         job.waitForCompletion(true);  
+        String old = printValue("hdfs://10.10.21.71:9000/test/new_words.txt", conf, "E:\\files\\", "words.txt");
+        String stat = printValue("hdfs://10.10.21.71:9000/test/stat/part-r-00000", conf, "E:\\files\\", "words.txt");
+        long start = job.getStartTime();
+        long end = job.getFinishTime();
+        System.out.println("总共耗时：" + (end - start) + " ms");
+        
+     
+        System.out.println("------------------原始数据----------------------------");
+        System.out.println(old);
+        
+        System.out.println("------------------统计数据----------------------------");
+        System.out.println(stat);
     }  
 }
